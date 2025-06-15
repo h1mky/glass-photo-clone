f6a53fe7a1eca1654b0bead603f70feb55790b17
Nikita Myroniuk <nikitamix1sstt@gmail.com>
2025-06-15 22:34:10 +0200

    handler-get-post-info-added

---
+++ internal/post/get_post_info_handler.go
@@ -0,0 +1,26 @@
+package post
+
+import (
+	"github.com/go-chi/chi/v5"
+	"glass-photo/internal/common"
+	"net/http"
+	"strconv"
+)
+
+func getPostByIDHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	postID, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, "wrong post ID", http.StatusBadRequest)
+		return
+	}
+
+	post, err := GetPostByID(ctx, postID)
+	if err != nil {
+		http.Error(w, "unavailiable to get post", http.StatusServiceUnavailable)
+		return
+	}
+
+	common.JsonEncode(w, post)
+}
