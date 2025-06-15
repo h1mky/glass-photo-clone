10ba07f5e23c3a99a519a80f8f7ebaa641e14a17
zvezd <adelevnikita@gmail.com>
2025-06-16 00:20:01 +0200

    fix posts_info query and struct

--- internal/post/get_post_info.go
+++ internal/post/get_post_info.go
@@ -15,22 +15,18 @@ func GetPostByID(ctx context.Context, postID int) ([]PostWithComments, error) {
 
 	query := `
         SELECT
-            p.id AS post_id,
-            p.title,
-            p.post_img,
-            p.created_at,
-            u.username AS post_author,
-            c.content AS comment_text,
-            c.created_at AS comment_created_at,
-            cu.id AS comment_user_id,
-            cu.username AS comment_author
-        FROM
-            posts AS p
-            JOIN users AS u ON p.user_id = u.id
-            LEFT JOIN comments AS c ON c.post_id = p.id
-            LEFT JOIN users AS cu ON c.user_id = cu.id
-        WHERE
-            p.id = $1`
+    p.id AS post_id,
+    p.title,
+    p.post_img,
+    p.created_at,
+    u.username AS post_author,
+    u.id AS post_author_id
+FROM
+    posts AS p
+    JOIN users AS u ON p.user_id = u.id
+WHERE
+    p.id = $1
+`
 
 	var post []PostWithComments
 	err := db.DB.SelectContext(ctx, &post, query, postID)
--- internal/post/model.go
+++ internal/post/model.go
@@ -10,13 +10,10 @@ type MainPagePost struct {
 }
 
 type PostWithComments struct {
-	ID             int        `db:"post_id" json:"id"`
-	Title          string     `db:"title" json:"title"`
-	PostImg        string     `db:"post_img" json:"post_img"`
-	CreatedAt      time.Time  `db:"created_at" json:"created_at"`
-	PostAuthor     string     `db:"post_author" json:"post_author"`
-	CommentText    *string    `db:"comment_text" json:"comment_text,omitempty"`
-	CommentCreated *time.Time `db:"comment_created_at" json:"comment_created_at,omitempty"`
-	CommentUserID  *int       `db:"comment_user_id" json:"comment_user_id,omitempty"`
-	CommentAuthor  *string    `db:"comment_author" json:"comment_author,omitempty"`
+	PostID       int       `db:"post_id"`
+	Title        string    `db:"title"`
+	PostImg      string    `db:"post_img"`
+	CreatedAt    time.Time `db:"created_at"`
+	PostAuthor   string    `db:"post_author"`
+	PostAuthorID int       `db:"post_author_id"`
 }
