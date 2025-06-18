e9b852b2253906d65e1cf3ea9268295d881ee71b
zvezd <adelevnikita@gmail.com>
2025-06-18 16:13:15 +0200

    add default pfp for new users

--- internal/auth/create_new_user_handler.go
+++ internal/auth/create_new_user_handler.go
@@ -10,6 +10,7 @@ func createNewUserHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
 	var user UserInputRegister
+	defaultUserPhoto := "https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg"
 
 	if err := common.JsonParse(w, r, &user); err != nil {
 		http.Error(w, "invalid json", http.StatusBadRequest)
@@ -22,6 +23,7 @@ func createNewUserHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	user.Password = common.HashPasswordSHA256(user.Password)
+	user.UserPhoto = defaultUserPhoto
 
 	if err := createNewUserPostgres(ctx, user); err != nil {
 		http.Error(w, fmt.Sprintf("error creating new user: %v", err), http.StatusInternalServerError)
--- internal/auth/create_new_user_postgres.go
+++ internal/auth/create_new_user_postgres.go
@@ -13,7 +13,7 @@ func createNewUserPostgres(ctx context.Context, register UserInputRegister) erro
 	defer cancel()
 
 	query := `
-INSERT INTO users (username, password,email) VALUES (:username, :password, :email)
+INSERT INTO users (username, password,email,user_img) VALUES (:username, :password, :email, :user_img)
 		`
 
 	if _, err := db.DB.NamedExecContext(ctx, query, register); err != nil {
