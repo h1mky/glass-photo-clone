f02bf7865fa2f54b560369f99f1a9b2bed514e32
zvezd <adelevnikita@gmail.com>
2025-06-18 14:19:36 +0200

    forgot add this in routes

--- internal/comment/route.go
+++ internal/comment/route.go
@@ -4,4 +4,5 @@ import "glass-photo/internal/router"
 
 func CommentRoute() {
 	router.R.Get("/comments/{id}", getAllCommentsHandler)
+	router.R.With(router.JWTMiddleware).Post("/comments/{id}", postCommentsHandler)
 }
